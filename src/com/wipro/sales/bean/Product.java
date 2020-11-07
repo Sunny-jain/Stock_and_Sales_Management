95214f350c1eef21762a7f600f36f243e0c91f12
Sunny-jain <sunny1388.cse18@chitkara.edu.in>
2020-11-07 11:53:48 +0530

    Stock class

---
+++ src/com/wipro/sales/bean/Product.java
@@ -0,0 +1,39 @@
+package com.wipro.sales.bean;
+
+public class Product {
+	public String productID;
+	public String productName;
+	public int quantityOnHand;
+	public double productUnitPrice;
+	public int reorderLevel;
+	public String getProductID() {
+		return productID;
+	}
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getQuantityOnHand() {
+		return quantityOnHand;
+	}
+	public void setQuantityOnHand(int quantityOnHand) {
+		this.quantityOnHand = quantityOnHand;
+	}
+	public double getProductUnitPrice() {
+		return productUnitPrice;
+	}
+	public void setProductUnitPrice(double productUnitPrice) {
+		this.productUnitPrice = productUnitPrice;
+	}
+	public int getReorderLevel() {
+		return reorderLevel;
+	}
+	public void setReorderLevel(int reorderLevel) {
+		this.reorderLevel = reorderLevel;
+	}
+}
